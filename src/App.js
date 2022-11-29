734ce898fad1750b250a1e22f315cd0e0d47c44e
AlejandroArielB <alejandroarielbarrionuevo@gmail.com>
2022-11-29 20:54:03 -0300

    Agrego un cohete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="images/falconx.jpeg" alt="cohete" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
